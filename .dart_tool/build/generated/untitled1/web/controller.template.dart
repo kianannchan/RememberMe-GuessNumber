e0dbcd9e2597fb1b7800e528cb7f844e98ce0706
kianannchan <kianannchan@gmail.com>
2020-05-24 03:24:25 +0800

    remove temp files

--- .dart_tool/build/generated/untitled1/web/controller.template.dart
+++
@@ -1,9 +0,0 @@
-// **************************************************************************
-// Generator: AngularDart Compiler
-// **************************************************************************
-
-import 'controller.dart';
-export 'controller.dart';
-
-// No initReflector() linking required.
-void initReflector() {}
